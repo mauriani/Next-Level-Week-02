2e3d386f97052c18d7a56e3a0296c5436db445c3
mauriani <maurianim@gmail.com>
2021-02-09 14:26:46 -0300

    alts

--- mobile/src/pages/Favorites/index.tsx
+++ mobile/src/pages/Favorites/index.tsx
@@ -1,10 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 import { View, ScrollView } from "react-native";
+import { useFocusEffect } from "@react-navigation/native";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+
 import PageHeader from "../../components/PageHeader";
-import TeacherItem from "../../components/TeacherItem";
+import TeacherItem, { Teacher } from "../../components/TeacherItem";
 import styles from "./styles";
 
 const Favorites: React.FC = () => {
+  const [favorites, setFavorites] = useState([]);
+
+  function loadFavorites() {
+    AsyncStorage.getItem("favorites").then((response) => {
+      if (response) {
+        const favoritesTeachers = JSON.parse(response);
+
+        setFavorites(favoritesTeachers);
+      }
+    });
+  }
+  // quando a aba troca ele atualiza
+  useFocusEffect(() => {
+    loadFavorites();
+  });
+
   return (
     <View style={styles.container}>
       <PageHeader title="Meus Proffys favoritos" />
@@ -15,7 +34,11 @@ const Favorites: React.FC = () => {
           paddingHorizontal: 16,
           paddingBottom: 16,
         }}
-      ></ScrollView>
+      >
+        {favorites.map((teacher: Teacher) => {
+          return <TeacherItem teacher={teacher} favorited={true} />;
+        })}
+      </ScrollView>
     </View>
   );
 };
